6f247b5a61a25ed30546425675442921a0901c66
jerePerez <jerengue@gmail.com>
2025-06-28 19:08:05 -0300

    prueba commit copilot

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("seeInvitationBtn").addEventListener("click", () => {
+    alert("Invitation will be shown!");
+    // You could also scroll to a section, open a modal, etc.
+});
